99dfd60e51e278a66b6973caa349616627d3c6f0
Joao Castilho <joaocastilho.work@gmail.com>
2026-02-13 00:38:34 +0000

    fix(build): conditionally compile winres only on windows

--- build.rs
+++ build.rs
@@ -1,21 +1,23 @@
+#[cfg(windows)]
 fn main() {
-    if cfg!(target_os = "windows") {
-        let mut res = winres::WindowsResource::new();
-        if std::path::Path::new("icon.ico").exists() {
-            res.set_icon("icon.ico");
-        }
-
-        // Set metadata for Task Manager / Startup
-        res.set("FileDescription", "Wallp - Wallpaper changer");
-        res.set("ProductName", "Wallp");
-        res.set("CompanyName", "Joao Castilho");
-        res.set("LegalCopyright", "Copyright (c) 2026 Joao Castilho");
-        res.set("InternalName", "wallp.exe");
-        res.set("OriginalFilename", "wallp.exe");
-        res.set("FileVersion", "1.0.0.0");
-        res.set("ProductVersion", "1.0.0.0");
-        res.set("AssemblyVersion", "1.0.0.0");
-
-        res.compile().unwrap();
+    let mut res = winres::WindowsResource::new();
+    if std::path::Path::new("icon.ico").exists() {
+        res.set_icon("icon.ico");
     }
+
+    // Set metadata for Task Manager / Startup
+    res.set("FileDescription", "Wallp - Wallpaper changer");
+    res.set("ProductName", "Wallp");
+    res.set("CompanyName", "Joao Castilho");
+    res.set("LegalCopyright", "Copyright (c) 2026 Joao Castilho");
+    res.set("InternalName", "wallp.exe");
+    res.set("OriginalFilename", "wallp.exe");
+    res.set("FileVersion", "1.0.0.0");
+    res.set("ProductVersion", "1.0.0.0");
+    res.set("AssemblyVersion", "1.0.0.0");
+
+    res.compile().unwrap();
 }
+
+#[cfg(not(windows))]
+fn main() {}
